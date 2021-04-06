605ec85a41f6d943e7aab84894a0e3d995a77d87
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-06 11:40:30 -0700

    add new email fields address and email object
    Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 // Business Logic for Address Book -------
 function AddressBook() {
   this.contacts = {};
@@ -31,30 +32,104 @@ AddressBook.prototype.deleteContact = function (id) {
 
 AddressBook.prototype.updateContact = function (contact) {
 
-  // first look up in contacts by the contact.id property
-  // example: contact.firstName used to be "Ada", now it's "Steve", but since id is same, it will reassign Ada to Steve.
   if (this.contacts[contact.id] === undefined) {
-    // if it doesn't exist, return false
     return false;
   }
-  // if it does exist, 
-  //we will reassign the object at key contact.id back to the contact with its new properties
-  // this will work, because the contact that is being passed as an arguement has the same id, and it is unique.
   this.contacts[contact.id] = contact;
 
 }
 
 // Business Logic for Contacts ----------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, emailAddresses, address) {
   this.firstName = firstName
   this.lastName = lastName
   this.phoneNumber = phoneNumber
+  this.emailAddresses = emailAddresses
+  this.address = address
+}
+
+function Email(address, type) {
+  this.address = address
+  this.type = type
 }
 
 Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
 
-let testContact = new Contact("Ada", "Lovelace", "503-555-1111");
-//console.log(testContact.fullName());
+// User interface logic
 
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+
+  Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+    contactsList.html(htmlForContactInfo);
+  });
+
+}
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  $(".address").html(contact.address);
+  contact.emailAddresses.forEach(function (email) {
+    console.log(email);
+    if (email.address) {
+      $(".email-addresses").append("<li>" + email.address + " : " + email.type + "</li>")
+    }
+  });
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function () {
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function () {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+
+$(document).ready(function () {
+  attachContactListeners();
+  $("form#new-contact").submit(function (event) {
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    const inputtedEmail1 = new Email($("input#new-email1").val(), $("#email-address1-types option:selected").text());
+    const inputtedEmail2 = new Email($("input#new-email2").val(), $("#email-address2-types option:selected").text());
+    const inputtedEmail3 = new Email($("input#new-email3").val(), $("#email-address3-types option:selected").text());
+    const emailAddresses = [inputtedEmail1, inputtedEmail2, inputtedEmail3];
+    const inputtedAddress = $("input#new-address").val();
+    let newContact = new Contact(
+      inputtedFirstName,
+      inputtedLastName,
+      inputtedPhoneNumber,
+      emailAddresses,
+      inputtedAddress);
+    if (newContact) {
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input#new-phone-number").val("");
+      $("input#new-email1").val("");
+      $("input#new-email2").val("");
+      $("input#new-email3").val("");
+      $("input#new-address").val("");
+      addressBook.addContact(newContact);
+    }
+    displayContactDetails(addressBook);
+    event.preventDefault();
+  });
+});
